d110656c3a701c0acf21d1fc34b3c086cd903dd4
= <info@josevicentecarratala.com>
2024-02-15 18:34:02 +0100

    enrutamiento

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { BlogComponent } from './blog/blog.component';
+import { SobremiComponent } from './sobremi/sobremi.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'inicio',component:InicioComponent},
+    {path:'blog',component:BlogComponent},
+    {path:'sobremi',component:SobremiComponent},
+    {path:'contacto',component:ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { PiedepaginaComponent } from './piedepagina/piedepagina.component';
