5bb55429a3fda6f46a1db3c35284011211297be7
= <info@josevicentecarratala.com>
2024-02-15 18:46:17 +0100

    Creación de la base de datos con blog

---
+++ Servidor/001-Mongo/003-Crear entradas.js
@@ -0,0 +1,17 @@
+db.entradas.insertMany([
+    {
+        'titulo':'Primera entrada',
+        'fecha':'2024-02-15',
+        'contenido':'Este es el contenido de la primera entrada'
+    },
+    {
+        'titulo':'Segunda entrada',
+        'fecha':'2024-02-16',
+        'contenido':'Este es el contenido de la segunda entrada'
+    },
+    {
+        'titulo':'Tercera entrada',
+        'fecha':'2024-02-17',
+        'contenido':'Este es el contenido de la tercera entrada'
+    }
+]);
