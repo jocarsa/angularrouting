877b54a8a2df0303255d9d21efb78168ee28fad6
= <info@josevicentecarratala.com>
2024-02-15 18:23:31 +0100

    cabecera y pie de pagina

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PiedepaginaComponent } from './piedepagina/piedepagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PiedepaginaComponent
   ],
   imports: [
     BrowserModule,
