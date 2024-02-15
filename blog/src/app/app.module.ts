d2dfc41fc0c7613ab91274d8fad86551798ecead
= <info@josevicentecarratala.com>
2024-02-15 18:27:52 +0100

    cuatro componentes como paginas

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { PiedepaginaComponent } from './piedepagina/piedepagina.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BlogComponent } from './blog/blog.component';
+import { SobremiComponent } from './sobremi/sobremi.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    PiedepaginaComponent
+    PiedepaginaComponent,
+    InicioComponent,
+    BlogComponent,
+    SobremiComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
